1eb5fba6e4c036c756255b2c149c4eb20a75793a
Andre Francez <andregfrancez@gmail.com>
2023-07-30 17:32:18 -0300

    Add Entity Framework, Migrations, Seed, and Model Updates

--- src/Models/Owner.cs
+++ src/Models/Owner.cs
@@ -1,7 +1,8 @@
 ﻿namespace PokemonApp.Models {
     public class Owner {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Gym { get; set; }
         public Country Country { get; set; }
         public ICollection<PokemonOwner> PokemonOwners { get; set; }
--- src/Models/Review.cs
+++ src/Models/Review.cs
@@ -1,8 +1,9 @@
-﻿]namespace PokemonApp.Models {
+﻿namespace PokemonApp.Models {
     public class Review {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
+        public int Rating { get; set; }
         public Reviewer Reviewer { get; set; }
         public Pokemon Pokemon { get; set; }
     }
