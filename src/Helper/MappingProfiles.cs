1b88cbdba219815802d7bb2d6da5d348d15dcaa5
Andre Francez <andregfrancez@gmail.com>
2023-08-25 20:05:07 -0300

    Implemented Reviewers get methods and Reviewer DTO

--- src/Helper/MappingProfiles.cs
+++ src/Helper/MappingProfiles.cs
@@ -13,6 +13,7 @@ namespace PokemonApp.Helper
             CreateMap<Country, CountryDto>();
             CreateMap<Owner, OwnerDto>();
             CreateMap<Review, ReviewDto>();
+            CreateMap<Reviewer, ReviewerDto>();
         }
     }
 }
---
+++ src/Interfaces/IReviewerRepository.cs
@@ -0,0 +1,12 @@
+﻿using PokemonApp.Models;
+
+namespace PokemonApp.Interfaces
+{
+    public interface IReviewerRepository
+    {
+        ICollection<Reviewer> GetReviewers();
+        Reviewer GetReviewer(int reviewerId);
+        ICollection<Review> GetReviewsByReviewer(int reviewerId);
+        bool ReviewerExists(int reviewerId);
+    }
+}
