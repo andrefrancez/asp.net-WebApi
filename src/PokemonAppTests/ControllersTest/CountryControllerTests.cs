27b940e241f305f1c232f3afa3524b49c90c0251
Andre Francez <andregfrancez@gmail.com>
2024-04-26 16:04:01 -0300

    refactor: Refactored CountryControllerTests and changed the return type of CreateCountry method in CountryController

--- src/PokemonAppTests/ControllersTest/CountryControllerTests.cs
+++ src/PokemonAppTests/ControllersTest/CountryControllerTests.cs
@@ -14,126 +14,192 @@ namespace PokemonAppTests.ControllersTest
     public class CountryControllerTests
     {
         private Fixture _fixture;
-        private Mock<ICountryRepository> _mockRepository;
+        private Mock<ICountryRepository> _mockCountryRepo;
         private Mock<IMapper> _mockMapper;
         private CountryController _controller;
 
         public CountryControllerTests()
         {
             _fixture = new Fixture();
-            _mockRepository = new Mock<ICountryRepository>();
+            _mockCountryRepo = new Mock<ICountryRepository>();
             _mockMapper = new Mock<IMapper>();
-            _controller = new CountryController(_mockRepository.Object, _mockMapper.Object);
+            _controller = new CountryController(_mockCountryRepo.Object, _mockMapper.Object);
         }
 
         [Fact]
         public void GetCountries_ReturnsOkResultWithCountries()
         {
+            // Arrange
             _fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
             _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
 
             var countries = _fixture.CreateMany<Country>(6).ToList();
 
-            _mockRepository.Setup(repository => repository.GetCountries()).Returns(countries);
-            _mockMapper.Setup(mapper => mapper.Map<List<CountryDto>>(countries)).Returns(countries.Select(c => new CountryDto
+            _mockCountryRepo.Setup(x => x.GetCountries()).Returns(countries);
+            _mockMapper.Setup(x => x.Map<List<CountryDto>>(countries)).Returns(countries.Select(c => new CountryDto
             {
                 Id = c.Id,
                 Name = c.Name,
             }).ToList());
 
+            // Act
             var result = _controller.GetCountries();
+
+            // Assert
             Assert.IsType<OkObjectResult>(result);
+
+            _mockCountryRepo
+                .Verify(x => x.GetCountries(), Times.Once);
+
+            _mockMapper
+                .Verify(x => x.Map<List<CountryDto>>(countries), Times.Once);
         }
 
         [Fact]
         public void GetCountry_ReturnsOkResultWithCountry()
         {
+            // Arrange
             _fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
             _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
 
             var country = _fixture.Create<Country>();
 
-            _mockRepository.Setup(repository => repository.CountryExists(country.Id)).Returns(true);
-            _mockRepository.Setup(repository => repository.GetCountry(country.Id)).Returns(country);
-            _mockMapper.Setup(mapper => mapper.Map<CountryDto>(country)).Returns((Country c) => new CountryDto
+            _mockCountryRepo.Setup(x => x.CountryExists(country.Id)).Returns(true);
+            _mockCountryRepo.Setup(x => x.GetCountry(country.Id)).Returns(country);
+            _mockMapper.Setup(x => x.Map<CountryDto>(country)).Returns((Country c) => new CountryDto
             {
                 Id = c.Id,
                 Name = c.Name,
             });
 
+            // Act
             var result = _controller.GetCountry(country.Id);
+
+            // Assert
             Assert.IsType<OkObjectResult>(result);
+
+            _mockCountryRepo
+                .Verify(x => x.CountryExists(country.Id), Times.Once);
+
+            _mockCountryRepo
+                .Verify(x => x.GetCountry(country.Id), Times.Once);
+
+            _mockMapper
+                .Verify(x => x.Map<CountryDto>(country), Times.Once);
         }
 
         [Fact]
         public void GetCountryByOwner_ReturnsOkResultWithCountry()
         {
+            // Arrange
             _fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
             _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
 
+            var owner = _fixture.Create<Owner>();
             var countries = _fixture.CreateMany<Country>().ToList();
-            var randomCountry = countries[new Random().Next(countries.Count)];
-            var owner = _fixture.Build<Owner>().Without(o => o.Id).With(o => o.Country, randomCountry).Create();
 
-            _mockRepository.Setup(repository => repository.GetCountryByOwner(It.IsAny<int>()))
-                   .Returns(randomCountry);
+            _mockCountryRepo.Setup(x => x.GetCountryByOwner(owner.Id)).Returns(It.IsAny<Country>());
 
-            _mockMapper.Setup(mapper => mapper.Map<CountryDto>(It.IsAny<Country>()))
-               .Returns((Country c) => new CountryDto
-               {
-                   Id = c.Id,
-                   Name = c.Name,
-               });
-
+            // Act
             var result = _controller.GetCountryByOwner(owner.Id);
+
+            // Assert
             Assert.IsType<OkObjectResult>(result);
+
+            _mockCountryRepo
+                .Verify(x => x.GetCountryByOwner(owner.Id), Times.Once);
         }
 
         [Fact]
         public void CreateCountry_ReturnsOkResultWithCountry()
         {
+            // Arrange
             _fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
             _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
 
-            var countryDto = _fixture.Create<CountryDto>();
+            var country = _fixture.Create<CountryDto>();
+            var countryMapped = _fixture.Build<Country>()
+                .With(c => c.Id, country.Id)
+                .With(c => c.Name, country.Name)
+                .Create();
 
-            _mockRepository.Setup(repository => repository.GetCountries()).Returns(new List<Country>());
-            _mockRepository.Setup(repository => repository.CreateCountry(It.IsAny<Country>())).Returns(true);
+            _mockCountryRepo.Setup(x => x.GetCountries()).Returns(new List<Country>());
+            _mockMapper.Setup(x => x.Map<Country>(country)).Returns(countryMapped);
+            _mockCountryRepo.Setup(x => x.CreateCountry(countryMapped)).Returns(true);
 
-            var result = _controller.CreateCountry(countryDto);
+            // Act
+            var result = _controller.CreateCountry(country);
 
-            var okResult = Assert.IsType<OkObjectResult>(result);
-            Assert.Equal("Successfully created", okResult.Value);
+            // Assert
+            Assert.IsType<CreatedResult>(result);
+
+            _mockCountryRepo
+                .Verify(x => x.GetCountries(), Times.Once);
+
+            _mockMapper
+                .Verify(x => x.Map<Country>(country), Times.Once);
+
+            _mockCountryRepo
+                .Verify(x => x.CreateCountry(countryMapped), Times.Once);
         }
 
         [Fact]
         public void UpdateCountry_ReturnsNoContentResult()
         {
+            // Arrange
             _fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
             _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
 
             var country = _fixture.Create<Country>();
-            var updatedCountry = _fixture.Build<CountryDto>().With(c => c.Id, country.Id).Create();
+            var CountryUpdate = _fixture.Build<CountryDto>().With(c => c.Id, country.Id).Create();
 
-            _mockRepository.Setup(repository => repository.CountryExists(country.Id)).Returns(true);
-            _mockRepository.Setup(repository => repository.UpdateCountry(It.IsAny<Country>())).Returns(true);
+            _mockCountryRepo.Setup(x => x.CountryExists(country.Id)).Returns(true);
+            _mockMapper.Setup(x => x.Map<Country>(CountryUpdate)).Returns(country);
+            _mockCountryRepo.Setup(x => x.UpdateCountry(It.IsAny<Country>())).Returns(true);
 
-            var result = _controller.UpdateCountry(country.Id, updatedCountry);
+            // Act
+            var result = _controller.UpdateCountry(country.Id, CountryUpdate);
+
+            // Assert
             Assert.IsType<NoContentResult>(result);
+
+            _mockCountryRepo
+                .Verify(x => x.CountryExists(country.Id), Times.Once);
+
+            _mockMapper
+                .Verify(x => x.Map<Country>(CountryUpdate), Times.Once);
+
+            _mockCountryRepo
+                .Verify(x => x.UpdateCountry(It.IsAny<Country>()), Times.Once);
         }
 
         [Fact]
         public void DeleteCountry_ReturnsNoContentResult()
         {
+            // Arrange
             _fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
             _fixture.Behaviors.Add(new OmitOnRecursionBehavior());
 
             var country = _fixture.Create<Country>();
 
-            _mockRepository.Setup(repository => repository.CountryExists(country.Id)).Returns(true);
+            _mockCountryRepo.Setup(x => x.CountryExists(country.Id)).Returns(true);
+            _mockCountryRepo.Setup(x => x.GetCountry(country.Id)).Returns(country);
+            _mockCountryRepo.Setup(x => x.DeleteCountry(country)).Returns(true);
 
+            // Act
             var result = _controller.DeleteCountry(country.Id);
+
+            // Assert
             Assert.IsType<NoContentResult>(result);
+
+            _mockCountryRepo
+                .Verify(x => x.CountryExists(country.Id), Times.Once);
+
+            _mockCountryRepo
+                .Verify(x => x.GetCountry(country.Id), Times.Once);
+
+            _mockCountryRepo
+                .Verify(x => x.DeleteCountry(country), Times.Once);
         }
     }
 }
